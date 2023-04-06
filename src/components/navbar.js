3405d20eaab486b868b6eb69eaee5242823e1ada
Arbenita Mani <arbenita.mani123@gmail.com>
2023-04-06 12:24:20 +0200

    nav bar added

---
+++ src/components/navbar.js
@@ -0,0 +1,23 @@
+import "./navbar.css";
+function Navbar(){
+    return(
+     <><nav>
+        
+        <div>
+    <ul id="navbar">
+        
+<li><a href="home.html">Home</a></li>
+<li><a href="home.html">Shop</a></li>
+<li><a href="home.html">Blog</a></li>
+<li><a href="home.html">About</a></li>
+<li><a href="home.html">Contact</a></li>
+
+
+    </ul>
+    </div>
+     </nav>
+
+     </>
+    );
+}
+export default Navbar;
