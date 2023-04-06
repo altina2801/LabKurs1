14c6097299e818c0b42b39b7cb856f41db1cec2c
Altina Hasani <ah60108@ubt-uni.net>
2023-04-06 03:51:33 -0700

    footer added

---
+++ src/components/footer.js
@@ -0,0 +1,59 @@
+import "./footer.css";
+
+
+
+function Footer(){
+    return(
+<div className="footer">
+<div className="top">
+    <div>
+        <h1>Tranquillo</h1>
+        <p>Choose your favourite Therapist</p>
+    </div>
+    <div>
+        <a href="image.png"><i className="fa-brands fa-facebook-square"></i></a>
+        <a href="/"><i className="fa-brands fa-instagram-square"></i></a>
+        <a href="/"><i className="fa-brands fa-twitter-square"></i></a>
+    </div>
+</div>
+
+
+<div className="bottom">
+   <div>
+    <h4>Project</h4>
+    <a href="/">Changelog</a>
+    <a href="/">Status</a>
+    <a href="/">License</a>
+    <a href="/">All Versions</a>
+
+    </div> 
+    <div>
+    <h4>Help</h4>
+    <a href="/">Support</a>
+    <a href="/">Troubleshooting</a>
+    <a href="/">Contact Us</a>
+    
+    </div> 
+    <div>
+    <h4>Community</h4>
+    <a href="/">GitHub</a>
+    <a href="/">Issues</a>
+    <a href="/">Project</a>
+    <a href="/">Twiiter</a>
+    
+    </div> 
+    <div>
+    <h4>Others</h4>
+    <a href="/">Terms of Services</a>
+    <a href="/">Privacy Policy</a>
+    <a href="/">License</a>
+    
+    </div> 
+</div>
+
+</div>
+
+
+    );
+}
+export default Footer;
