0b3702a142e97e9206caa2bf7de3f10a982ec29d
Arbenita Mani <arbenita.mani123@gmail.com>
2023-05-12 21:02:26 +0200

    Changes On The Design Of Sessions

--- front/src/components/Sessions.js
+++ front/src/components/Sessions.js
@@ -1,72 +1,97 @@
 import React from 'react';
 import '../css/Sessions.css';
-
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faPlus, faEdit, faTrash } from '@fortawesome/free-solid-svg-icons';
 
 const Sessions = () => {
-    return (
-      <div className="main-container">
-        <div className="left-container">
-          <button className="create-session-btn">
-            <span className="create-session-icon">+</span>
-            Create New Session
-          </button>
-        </div>
-        <div className="right-container">
-          <div className="last-session-btn-container">
-            <button className="last-session-btn">Last Session</button>
-            <div className="last-session-btn-options">
-              <button className="edit-session-btn">Edit</button>
-              <button className="delete-session-btn">Trash</button>
+  return (
+    <div className="main-container">
+      <div className="left-container">
+      <button className="create-session-btn">
+       <FontAwesomeIcon icon={faPlus} className="create-session-icon" />
+    Create New Session
+  </button>
+      </div>
+      <div className="right-container">
+        <div className="last-session-btn-container">
+          <p className="last-session-btn-text">Last Session</p>
+          <div className="last-session-btn-options">
+          <div className="edit-session-btn">
+          <FontAwesomeIcon icon={faEdit} />
             </div>
+            <div className="delete-session-btn" role="button">
+           <FontAwesomeIcon icon={faTrash} />
           </div>
-        </div>
-        <div className="right-container">
-          <div className="last-session-btn-container">
-            <button className="last-session-btn">Last Session</button>
-            <div className="last-session-btn-options">
-              <button className="edit-session-btn">Edit</button>
-              <button className="delete-session-btn">Trash</button>
-            </div>
-          </div>
-        </div>
-        <div className="right-container">
-          <div className="last-session-btn-container">
-            <button className="last-session-btn">Last Session</button>
-            <div className="last-session-btn-options">
-              <button className="edit-session-btn">Edit</button>
-              <button className="delete-session-btn">Trash</button>
-            </div>
-          </div>
-        </div>
-        <div className="right-container">
-          <div className="last-session-btn-container">
-            <button className="last-session-btn">Last Session</button>
-            <div className="last-session-btn-options">
-              <button className="edit-session-btn">Edit</button>
-              <button className="delete-session-btn">Trash</button>
-            </div>
-          </div>
-        </div>
-        <div className="right-container">
-          <div className="last-session-btn-container">
-            <button className="last-session-btn">Last Session</button>
-            <div className="last-session-btn-options">
-              <button className="edit-session-btn">Edit</button>
-              <button className="delete-session-btn">Trash</button>
-            </div>
-          </div>
-        </div>
-        <div className="right-container">
-          <div className="last-session-btn-container">
-            <button className="last-session-btn">Last Session</button>
-            <div className="last-session-btn-options">
-              <button className="edit-session-btn">Edit</button>
-              <button className="delete-session-btn">Trash</button>
-            </div>
           </div>
         </div>
       </div>
-    );
-  };
-  
-  export default Sessions;
+      <div className="right-container">
+        <div className="last-session-btn-container">
+          <p className="last-session-btn-text">Last Session</p>
+          <div className="last-session-btn-options">
+          <div className="edit-session-btn">
+          <FontAwesomeIcon icon={faEdit} />
+            </div>
+            <div className="delete-session-btn" role="button">
+           <FontAwesomeIcon icon={faTrash} />
+          </div>
+          </div>
+        </div>
+      </div>
+      <div className="right-container">
+        <div className="last-session-btn-container">
+          <p className="last-session-btn-text">Last Session</p>
+          <div className="last-session-btn-options">
+          <div className="edit-session-btn">
+          <FontAwesomeIcon icon={faEdit} />
+            </div>
+            <div className="delete-session-btn" role="button">
+           <FontAwesomeIcon icon={faTrash} />
+          </div>
+          </div>
+        </div>
+      </div>
+      <div className="right-container">
+        <div className="last-session-btn-container">
+          <p className="last-session-btn-text">Last Session</p>
+          <div className="last-session-btn-options">
+          <div className="edit-session-btn">
+          <FontAwesomeIcon icon={faEdit} />
+            </div>
+            <div className="delete-session-btn" role="button">
+           <FontAwesomeIcon icon={faTrash} />
+          </div>
+          </div>
+        </div>
+      </div>
+      <div className="right-container">
+        <div className="last-session-btn-container">
+          <p className="last-session-btn-text">Last Session</p>
+          <div className="last-session-btn-options">
+          <div className="edit-session-btn">
+          <FontAwesomeIcon icon={faEdit} />
+            </div>
+            <div className="delete-session-btn" role="button">
+           <FontAwesomeIcon icon={faTrash} />
+          </div>
+          </div>
+        </div>
+      </div>
+      <div className="right-container">
+        <div className="last-session-btn-container">
+          <p className="last-session-btn-text">Last Session</p>
+          <div className="last-session-btn-options">
+          <div className="edit-session-btn">
+          <FontAwesomeIcon icon={faEdit} />
+            </div>
+            <div className="delete-session-btn" role="button">
+           <FontAwesomeIcon icon={faTrash} />
+          </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Sessions;
