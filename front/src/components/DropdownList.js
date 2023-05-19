94eafc6f001119fe6db88c5104b2c00452aab932
Arbenita Mani <arbenita.mani123@gmail.com>
2023-05-19 14:30:54 +0200

    List Added On the Dropdown

--- front/src/components/DropdownList.js
+++ front/src/components/DropdownList.js
@@ -14,14 +14,33 @@ const DropdownList = () => {
     <div className="dropdown">
       <div className={`dropdown-header ${isOpen ? 'open' : ''}`} onClick={handleDropdownClick}>
         <div className={`triangle ${isTriangleInverted ? 'inverted' : ''}`} />
-        <p>Categories</p>
+        <p>Browse By</p>
       </div>
       {isOpen && (
         <ul className="dropdown-menu">
-          <li>- Clinical Psychologists</li>
-          <li>- Psychiatrists</li>
-          <li>- Counseling Psychologists</li>
-          <li>- Marriage and Family Therapists</li>
+          <ul className='subcategory-header'>
+            Categories
+            <ul className="subcategory-menu">
+              <li>Psychiatrists</li>
+              <li>Counseling Psychologists</li>
+              <li>Marriage and Family Therapists</li>
+            </ul>
+          </ul>
+          <ul className='subcategory-header'>
+            Location
+            <ul className="subcategory-menu">
+              <li>USA</li>
+              <li>EUROPE</li>
+              <li>ASIA</li>
+            </ul>
+          </ul>
+          <ul className='subcategory-header'>
+            Price
+            <ul className="subcategory-menu">
+              <li>Low to High</li>
+              <li>High to Low</li>
+            </ul>
+          </ul>
         </ul>
       )}
     </div>
